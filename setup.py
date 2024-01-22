3171d54b868f37f60c8967a5659f751efbcf7f5b
Mark Seo <markseo0424@gmail.com>
2024-01-23 02:11:51 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyRLFramework',
+    name='RLFramework',
     version='0.1.0',
     packages=find_packages(include=['RLFramework', 'RLFramework.*']),
     install_requires=[
