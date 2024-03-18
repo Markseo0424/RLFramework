aa6c2d23fc4b9c3557c422308245d5dd36cd56a3
Mark Seo <markseo0424@gmail.com>
2024-03-18 17:03:10 +0900

    RLFramework 0.9.0 released

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='RLFramework',
-    version='0.3.3',
+    version='0.9.0',
     packages=find_packages(include=['RLFramework', 'RLFramework.*']),
     url="https://github.com/Markseo0424/RLFramework.git",
     install_requires=[
         'numpy',
-        'torch'
+        'torch',
+        'matplotlib'
     ]
 )
